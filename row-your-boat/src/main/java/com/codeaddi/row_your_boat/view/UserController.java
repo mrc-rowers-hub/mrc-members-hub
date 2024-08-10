91e8d30d76c5d925fd0a3d42b7b18527dac60b2a
AdelaideBaron <baronadelaide@hotmail.com>
2024-08-10 19:15:20 +0100

    mapping to home page after login

--- row-your-boat/src/main/java/com/codeaddi/row_your_boat/view/UserController.java
+++ row-your-boat/src/main/java/com/codeaddi/row_your_boat/view/UserController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
@@ -12,6 +14,12 @@ public class UserController {
     @GetMapping("/")
     public String index(Model model) {
         return "login";
+    }
+
+    @PostMapping("/home")
+    public String home(@RequestParam("username") String username, Model model) {
+        model.addAttribute("username", username);
+        return "home";
     }
 
 }
