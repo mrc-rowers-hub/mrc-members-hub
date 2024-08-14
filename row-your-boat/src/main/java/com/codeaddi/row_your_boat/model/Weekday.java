f2daee049bec953225de13ce42376bf899f0c50c
AdelaideBaron <baronadelaide@hotmail.com>
2024-08-14 11:51:17 +0100

    sorting the sessions

--- row-your-boat/src/main/java/com/codeaddi/row_your_boat/model/Weekday.java
+++ row-your-boat/src/main/java/com/codeaddi/row_your_boat/model/Weekday.java
@@ -1,16 +1,24 @@
 package com.codeaddi.row_your_boat.model;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+@Getter
 @Slf4j
 public enum Weekday {
-  MONDAY,
-  TUESDAY,
-  WEDNESDAY,
-  THURSDAY,
-  FRIDAY,
-  SATURDAY,
-  SUNDAY;
+  MONDAY(1),
+  TUESDAY(2),
+  WEDNESDAY(3),
+  THURSDAY(4),
+  FRIDAY(5),
+  SATURDAY(6),
+  SUNDAY(7);
+
+  private final int dayNumber;
+
+  Weekday(int dayNumber) {
+    this.dayNumber = dayNumber;
+  }
 
   public static Weekday fromString(String string){
     try{
--- row-your-boat/src/main/java/com/codeaddi/row_your_boat/view/display/ViewService.java
+++ row-your-boat/src/main/java/com/codeaddi/row_your_boat/view/display/ViewService.java
@@ -5,6 +5,8 @@ import com.codeaddi.row_your_boat.controller.sessions.http.SchedulerClient;
 import com.codeaddi.row_your_boat.model.Squad;
 import com.codeaddi.row_your_boat.model.sessions.RowingSessions;
 import com.codeaddi.row_your_boat.model.sessions.http.RowingSession;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,11 @@ public class ViewService {
     List<RowingSession> rowingSessions = schedulerClient.getAllSessions();
     List<RowingSessions> groupedSessions =
         SessionsService.mapRowingSessionToSessions(rowingSessions);
-    return SessionsService.getRowingSessionsPerSquad(groupedSessions);
+    Map<Squad, List<RowingSessions>> standardSessions = SessionsService.getRowingSessionsPerSquad(groupedSessions);
+
+    standardSessions.forEach((key, value) -> value.sort(Comparator.comparing(rowingSession -> rowingSession.getDay().ordinal())));
+
+    return standardSessions;
+
   }
 }
